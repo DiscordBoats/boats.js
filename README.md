62e099975cad63ecb59b1d0f9e4a6ed4fafd5c17
Roy <root@mrsheldon.me>
2018-09-25 23:17:54 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# discordboats
-An NPM package for Discordboats.xyz
+[![NPM](https://nodei.co/npm/boats.js.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/boats.js/)
+
+# Getting Started
+Simply Run `npm install boats.js`
+
+## Usage
+
+**Example of posting a bots servercount to the API**
+
+```javascript
+const BOATS = require("boats.js");
+const boats = new boats('Your Token');
+boats.postStats('your server count', 'your bot id');
+```
+
+**Example of getting bot info**
+
+```javascript
+const BOATS = require("boats.js");
+const boats = new boats('Your Token');
+boats.getBot('bot-id');
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const snekfetch = require('snekfetch');
+const API = 'https://discordboats.xyz/api/';
+
+class BOATSAPI {
+  constructor(token) {
+    this.token = token;
+  }
+
+  _request(method, endpoint, data, auth) {
+    const request = snekfetch[method](API + endpoint);
+    if (method === 'post' && data) request.send(data);
+    if (method === 'get' && data) request.query(data);
+    if (auth) request.set({ Authorization: this.token });
+    return request;
+  }
+
+  async postStats(serverCount, id) {
+    if (!this.token) throw new Error('This function requires a token to be set');
+    if (!serverCount && !this.client) throw new Error('postStats requires 2 argument');
+    if (!id) throw new Error('Please enter a bot id for posting count');
+    const data = {};
+    data.server_count = serverCount;
+    const response = await this._request('post', `bot/${id}`, data, true);
+    return response.body;
+  }
+
+  async getBot(id) {
+    if (!id && !this.client) throw new Error('getBot requires id as argument');
+    if (!id) id = this.client.user.id;
+    const response = await this._request('get', `bot/${id}`);
+    const info = JSON.parse(response.text);
+    return info;
+  }
+}
+
+module.exports = BOATSAPI;
