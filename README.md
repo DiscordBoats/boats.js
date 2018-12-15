edf6c7d87a57c6eda697c45b1d0b532e156b91ba
David Ralph <ohlookitsderpy@protonmail.com>
2018-12-15 16:18:38 +0000

    Improvements

--- README.md
+++ README.md
@@ -1,45 +1,45 @@
 [![NPM](https://nodei.co/npm/boats.js.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/boats.js/)
 
 # Getting Started
-Simply Run `npm install boats.js@latest`
+Simply run `npm install boats.js@latest`
 
 ## Usage
 
-**Example of posting a bots servercount to the API**
+**Example of posting bot servercount**
 
 ```javascript
-const BOATS = require("boats.js")
-const Boats = new BOATS("API TOKEN")
+const BOATS = require("boats.js");
+const Boats = new BOATS("API TOKEN");
 
 Boats.postStats(SERVER_COUNT, BOT_ID).then(() => {
-    console.log('Successfully updated server count.')
+    console.log('Successfully updated server count.');
 }).catch((err) => {
-    console.error(err)
-})
+    console.error(err);
+});
 ```
 
 **Example of getting bot info**
 
 ```javascript
-const BOATS = require("boats.js")
-const Boats = new BOATS("API TOKEN")
+const BOATS = require("boats.js");
+const Boats = new BOATS("API TOKEN");
 
 Boats.getBot('BOT_ID').then(bot => {
-    console.log(bot)
+    console.log(bot);
 }).catch((err) => {
-    console.error(err)
-})
+    console.error(err);
+});
 ```
 
 **Example of getting user info**
 
 ```javascript
-const BOATS = require("boats.js")
-const Boats = new BOATS("API TOKEN")
+const BOATS = require("boats.js");
+const Boats = new BOATS("API TOKEN");
 
 Boats.getUser('USER_ID').then(user => {
-    console.log(user)
+    console.log(user);
 }).catch((err) => {
-    console.error(err)
-}) 
+    console.error(err);
+});
 ```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const snekfetch = require('snekfetch');
+const { get, post } = require('snekfetch');
 const Error = require('./ErrorHandler')
 const API = 'https://discord.boats/api/';
 
-class BOATSAPI {
+modules.exports = class BOATSAPI {
   constructor(token) {
     this.token = token;
   }
@@ -10,7 +10,7 @@ class BOATSAPI {
   getBot(botid) {
     if (typeof botid != 'string') throw new TypeError('Bot ID must be a string')
     return new Promise((resolve, reject) => {
-      snekfetch.get(API + '/bot/' + botid).then((req) => {
+      get(API + '/bot/' + botid).then((req) => {
         resolve(req.body);
       }).catch((error) => {
         reject(new Error(error));
@@ -18,10 +18,10 @@ class BOATSAPI {
     });
   }
 
-  getUser(botid) {
-    if (typeof botid != 'string') throw new TypeError('User ID must be a string')
+  getUser(userid) {
+    if (typeof userid != 'string') throw new TypeError('User ID must be a string')
     return new Promise((resolve, reject) => {
-      snekfetch.get(API + '/user/' + botid).then((req) => {
+      get(API + '/user/' + userid).then((req) => {
         resolve(req.body);
       }).catch((error) => {
         reject(new Error(error));
@@ -33,7 +33,7 @@ class BOATSAPI {
     if (typeof servercount != 'number') throw new TypeError('Server count must be a number.');
     if (typeof botid != 'string') throw new TypeError('Bot ID must be a string.');
     return new Promise((resolve, reject) => {
-      snekfetch.post(API + '/bot/' + botid).set('Authorization', this.token).send({
+      post(API + '/bot/' + botid).set('Authorization', this.token).send({
         server_count: servercount
       }).then((req) => {
         resolve(req.body);
@@ -43,5 +43,3 @@ class BOATSAPI {
     });
   }
 }
-
-module.exports = BOATSAPI;
